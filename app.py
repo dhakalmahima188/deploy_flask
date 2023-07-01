901a3f8c67a06d6a93ad37ae6290387442917951
mahima <dhakalmahima18@gmail.com>
2023-07-01 15:32:27 +0545

    running with .json

--- app.py
+++ app.py
@@ -1,52 +1,60 @@
+import json
+import time
 from flask import Flask, render_template, request, url_for, redirect
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 app = Flask(__name__)
-# connecting to Mongodb server
-#client = MongoClient("mongodb://mongo:irxi1GrdztU7pHfU4lFp@containers-us-west-23.railway.app:6674")
-client=MongoClient("mongodb://localhost:27017/")
 
-# accessing the "flask_db"
-db = client.flask_db
-
-# accessing the collection "todos" in "flask_db"
-todos = db.todos
-
+try:
+    with open('./todos.json', 'r') as f:
+        todos = json.load(f)
+except Exception as e:
+    todos = {}
+    
+def save_to_todos(todos):
+    with open('todos.json', 'w') as f:
+        json.dump(todos, f, indent=4)
+        
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
         content = request.form['content']
         important = 'important' in request.form
-        todos.insert_one({'content':content, 'important':important})
+        todos[str(int(time.time()))] = {'_id':str(int(time.time())),'content':content, 'important':important}
+        save_to_todos(todos)
         return redirect(url_for('index'))
-    all_todos = todos.find()
+    all_todos = todos.values()
     return render_template('index.html', todos=all_todos)
 
 
 
 @app.route('/<id>/delete/', methods = ['POST'])
 def delete_todo(id):
-    todos.delete_one({"_id":ObjectId(id)})
+    del todos[id]
+    save_to_todos(todos)
     return redirect(url_for('index'))
 
-@app.route('/<id>/update/', methods = ['GET','POST'])
+@app.route('/<id>/update/', methods = ['GET', 'POST'])
 def update_todo(id):
     if request.method == 'GET':
-        to_be_updated_todo = todos.find_one({'_id':ObjectId(id)})
+        to_be_updated_todo = todos[id]
         return render_template('update.html', todo=to_be_updated_todo)
-    else:
+    elif request.method == 'POST':
         content = request.form['content']
         important = 'important' in request.form
-        todos.find_one_and_update({'_id':ObjectId(id)},{'$set':{'content':content,'important':important}})
+        todos[id].update({'content':content, 'important':important})
+        save_to_todos(todos)
         return redirect(url_for('index'))
+    
+
 
 @app.route('/<id>/complete/', methods=['POST'])
 def mark_completed(id):
-    todos.find_one_and_update({'_id':ObjectId(id)},{'$set':{'completed':True}})
+    todos[id].update({'completed':True})
+    save_to_todos(todos)
     return redirect(url_for('index'))
 
 @app.route('/<id>/unmark/', methods=['POST'])
 def mark_uncompleted(id):
-    todos.find_one_and_update({'_id':ObjectId(id)},{'$set':{'completed':False}})
+    todos[id].update({'completed':False})
+    save_to_todos(todos)
     return redirect(url_for('index'))
--- app2.py
+++ app2.py
@@ -1,60 +1,54 @@
-import json
-import time
 from flask import Flask, render_template, request, url_for, redirect
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+# connecting to Mongodb server
+client = MongoClient("mongodb://irxi1GrdztU7pHfU4lFp:irxi1GrdztU7pHfU4lFp@containers-us-west-23.railway.app:6674")
+#client=MongoClient("mongodb://localhost:27017/")
 
-try:
-    with open('./todos.json', 'r') as f:
-        todos = json.load(f)
-except Exception as e:
-    todos = {}
-    
-def save_to_todos(todos):
-    with open('todos.json', 'w') as f:
-        json.dump(todos, f, indent=4)
-        
+# accessing the "flask_db"
+db = client.flask_db
+
+# accessing the collection "todos" in "flask_db"
+todos = db.todos
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
         content = request.form['content']
         important = 'important' in request.form
-        todos[str(int(time.time()))] = {'_id':str(int(time.time())),'content':content, 'important':important}
-        save_to_todos(todos)
+        todos.insert_one({'content':content, 'important':important})
         return redirect(url_for('index'))
-    all_todos = todos.values()
+    all_todos = todos.find()
     return render_template('index.html', todos=all_todos)
 
 
 
 @app.route('/<id>/delete/', methods = ['POST'])
 def delete_todo(id):
-    del todos[id]
-    save_to_todos(todos)
+    todos.delete_one({"_id":ObjectId(id)})
     return redirect(url_for('index'))
 
-@app.route('/<id>/update/', methods = ['GET', 'POST'])
+
+
+@app.route('/<id>/update/', methods = ['GET','POST'])
 def update_todo(id):
     if request.method == 'GET':
-        to_be_updated_todo = todos[id]
+        to_be_updated_todo = todos.find_one({'_id':ObjectId(id)})
         return render_template('update.html', todo=to_be_updated_todo)
-    elif request.method == 'POST':
+    else:
         content = request.form['content']
         important = 'important' in request.form
-        todos[id].update({'content':content, 'important':important})
-        save_to_todos(todos)
+        todos.find_one_and_update({'_id':ObjectId(id)},{'$set':{'content':content,'important':important}})
         return redirect(url_for('index'))
-    
-
 
 @app.route('/<id>/complete/', methods=['POST'])
 def mark_completed(id):
-    todos[id].update({'completed':True})
-    save_to_todos(todos)
+    todos.find_one_and_update({'_id':ObjectId(id)},{'$set':{'completed':True}})
     return redirect(url_for('index'))
 
 @app.route('/<id>/unmark/', methods=['POST'])
 def mark_uncompleted(id):
-    todos[id].update({'completed':False})
-    save_to_todos(todos)
+    todos.find_one_and_update({'_id':ObjectId(id)},{'$set':{'completed':False}})
     return redirect(url_for('index'))
